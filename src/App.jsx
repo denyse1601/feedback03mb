7e2160ba5ba5ab675fe3724ee6690a659f594d86
denyse.lima.istiliana <denyse.lima.istiliana@gmail.com>
2025-04-15 19:44:40 +0000

    mudança da função handlemudarnota para o componente inputbutton pois é responsabilidade dele

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ export function App() {
 
   return(
     submited === false ? (
-      <CardInicial handleMudarNotaAvaliacao={handleMudarNotaAvaliacao} handleSubmit={handleSubmit}/>
+      <CardInicial setNotaAvaliacao={setNotaAvaliacao} handleSubmit={handleSubmit}/>
     ) : (
       <CardFinal notaAvaliacao={notaAvaliacao}/>
    )
